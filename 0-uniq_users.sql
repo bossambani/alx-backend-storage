0c023df81f743f1cb9e1ed712a75c7dbed225395
bossambani <bossambaka@gmail.com>
2024-10-17 09:06:13 +0300

    Deleted a file

--- 0-uniq_users.sql
+++
@@ -1,8 +0,0 @@
--- Script that creates a table user
--- if it doesn't exist
-
-CREATE TABLE IF NOT EXISTS users(
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-);
