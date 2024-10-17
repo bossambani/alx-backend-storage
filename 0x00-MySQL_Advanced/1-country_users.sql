d9c8a786b0e206ff4bc649aab2a0513613addff2
bossambani <bossambaka@gmail.com>
2024-10-17 18:13:53 +0300

    country ENUMS

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,10 +1,10 @@
 -- A script that creates a table users with the following 
 -- requirements:
 
---id, integer, never null, auto increment and primary key
---email, string (255 characters), never null and unique
---name, string (255 characters)
---country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
 -- if it doesnt exist
 
 CREATE TABLE IF NOT EXISTS users(
