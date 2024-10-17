35326ff6024c0fa5dda86e45deb024c2c6cdfe4e
bossambani <bossambaka@gmail.com>
2024-10-17 08:56:01 +0300

    table users

--- 0-uniq_users.sql
+++ 0-uniq_users.sql
@@ -1,6 +1,8 @@
---Script that creates a table user
+-- Script that creates a table user
+-- if it doesn't exist
+
 CREATE TABLE IF NOT EXISTS users(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
