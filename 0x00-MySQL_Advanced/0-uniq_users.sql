d63a4924a92aab22a9f551f7b0fe517abec47f86
bossambani <bossambaka@gmail.com>
2024-10-17 09:14:41 +0300

    Table users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
---Script that creates a table user
---If it doesn't exist
+-- A script that creates a table users
+-- if it doesnt exist
 
 CREATE TABLE IF NOT EXISTS users(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
 );
