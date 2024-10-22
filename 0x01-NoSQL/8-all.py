58c37aaf927ca65f6e1cf8d2a5155cffc000a5d2
bossambani <bossambaka@gmail.com>
2024-10-22 21:35:11 +0300

    lists all documents in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import pymongo
+"""
+function that lists all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    return the documents in the collection
+    """
+    doc = mongo_collection.find()
+
+    if not doc:
+        return []
+
+    return mongo_collection.find()                                  
